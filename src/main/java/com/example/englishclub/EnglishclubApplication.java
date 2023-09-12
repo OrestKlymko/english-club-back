e955fe773d9a8e2f0ac846dba164bdb5641cf8a2
Orest Klymko <116631168+OrestKlymko@users.noreply.github.com>
2023-09-12 07:57:33 +0200

    create app

---
+++ src/main/java/com/example/englishclub/EnglishclubApplication.java
@@ -0,0 +1,13 @@
+package com.example.englishclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishclubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/englishclub/EnglishclubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.englishclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishclubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
